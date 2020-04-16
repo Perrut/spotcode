b8568d4510f030f3abd26aff9d2f25a7f1e71bc2
Matheus Perrut <matheus.perrut2302@gmail.com>
2020-04-16 00:25:13 -0300

    - Integração com React parte 1

---
+++ app/javascript/screens/discovery/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/common/section_wrapper'
+import NavbarFooter from '../../components/common/navbar_footer';
+import Discovery from '../../components/common/discovery';
+
+const DiscoveryScreen = () => {
+  return (
+    <Fragment>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
+      <NavbarFooter />
+    </Fragment>
+  );
+}
+export default DiscoveryScreen;
